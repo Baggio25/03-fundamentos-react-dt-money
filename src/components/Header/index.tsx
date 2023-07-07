f8021fe359d21faa2f11d965800cba474229d783
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-07-07 14:20:31 -0300

    Trasition Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
+import { NewTransactionModal } from '../NewTransactionModal';
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import logoImg from "../../assets/logo.svg";
-
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
   return (
@@ -9,7 +11,12 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} />
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal/>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
