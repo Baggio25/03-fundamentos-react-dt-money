2b6acb9ff62ac48a96b70c83b63b1358882d9d08
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-07-10 13:09:16 -0300

    Formatters

--- src/pages/Trasactions/index.tsx
+++ src/pages/Trasactions/index.tsx
@@ -4,14 +4,13 @@ import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 import {
   PriceHighlight,
   TransactionsContainer,
   TransactionsTable,
 } from "./styles";
-
-
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext);
@@ -31,11 +30,12 @@ export function Transactions() {
                   <td width="40%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === "outcome" && "- "}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               );
             })}
